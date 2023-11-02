34c82108401c3f297fc42ba78eb614e56cad0b6c
bstawy <125200508@sha.edu.eg>
2023-11-02 14:17:22 +0200

    add splash view route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies/core/theme/application_theme.dart';
 import 'package:movies/layout/home_layout.dart';
+import 'package:movies/pages/splash/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,18 +10,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Movies',
       theme: ApplicationTheme.theme,
-      initialRoute: HomeLayout.routeName,
+      initialRoute: SplashView.routeName,
       routes: {
+        SplashView.routeName: (context) => const SplashView(),
         HomeLayout.routeName: (context) => const HomeLayout(),
       },
     );
   }
 }
-
