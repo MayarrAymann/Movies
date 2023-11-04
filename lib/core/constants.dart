1375abc7b4b21b154ab0059e4f961bd6a0d79cfb
basta <125200508@sha.edu.eg>
2023-11-05 00:07:16 +0200

    add baseURL and apiKey values

---
+++ lib/core/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String baseURL = 'api.themoviedb.org';
+
+  static const String apiKey =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YmZhZDhiM2UxMjIxYTE5N2EyYmM2NjM1NGU2OGMwYyIsInN1YiI6IjY1NDZhYzEzOWNjNjdiMDBhYjQ2ZTU1YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y1TwS-eL6QQT1wdKFhjoGzodGJ4icmfp8TcwKOxLQEc';
+}
