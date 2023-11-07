b49779a68e5b42abc256ac7f83b89c10269fb716
basta <125200508@sha.edu.eg>
2023-11-07 12:48:50 +0200

    finish browse view tab

--- lib/pages/browse/browse_view.dart
+++ lib/pages/browse/browse_view.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:movies/pages/browse/widgets/category_item.dart';
+
+import '../../models/category_model.dart';
 
 class BrowseView extends StatelessWidget {
-  const BrowseView({super.key});
+  BrowseView({super.key});
+
+  List<CategoryModel> categories = [
+    CategoryModel(id: 1, name: 'Action'),
+    CategoryModel(id: 2, name: 'Romantic'),
+    CategoryModel(id: 3, name: 'Comedy'),
+    CategoryModel(id: 4, name: 'Scientific'),
+    CategoryModel(id: 5, name: 'Adult'),
+    CategoryModel(id: 1, name: 'Action'),
+    CategoryModel(id: 2, name: 'Romantic'),
+    CategoryModel(id: 3, name: 'Comedy'),
+    CategoryModel(id: 4, name: 'Scientific'),
+    CategoryModel(id: 5, name: 'Adult'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Center(
-          child: Text(
-            'Browse View',
+    return Padding(
+      padding: const EdgeInsets.only(top: 70, left: 15, right: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            'Browse Category',
             style: TextStyle(
-                color: Color(0xffB5B4B4),
-                fontSize: 24,
-                fontWeight: FontWeight.bold),
+              color: Colors.white,
+              fontFamily: 'Inter',
+              fontSize: 22,
+              fontWeight: FontWeight.w400,
+            ),
           ),
-        )
-      ],
+          const SizedBox(
+            height: 8,
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 7 / 4,
+                mainAxisSpacing: 35,
+                crossAxisSpacing: 35,
+              ),
+              itemBuilder: (context, index) =>
+                  CategoryItem(model: categories[index]),
+              itemCount: categories.length,
+              padding: const EdgeInsets.symmetric(vertical: 10),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
