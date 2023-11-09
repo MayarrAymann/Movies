193c264df27ff9b87e217f785df381ebf2a6ba0f
basta <125200508@sha.edu.eg>
2023-11-09 20:01:16 +0200

    add function to fetch categories

--- lib/core/network_layer/api_manager.dart
+++ lib/core/network_layer/api_manager.dart
@@ -1,3 +1,27 @@
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+import 'package:movies/core/constants.dart';
 
-class ApiManager {}
+import '../../models/category_model.dart';
+
+class ApiManager {
+  static Future<CategoryModel> fetchCategories() async {
+    Uri uri = Uri.https(
+      Constants.baseURL,
+      Constants.categoriesEndPoint,
+      <String, String>{
+        'api_key': Constants.apiKey,
+      },
+    );
+
+    var response = await http.get(uri);
+
+    print(jsonDecode(response.body));
+
+    CategoryModel categoryModel =
+        CategoryModel.fromJson(jsonDecode(response.body));
+
+    return categoryModel;
+  }
+}
