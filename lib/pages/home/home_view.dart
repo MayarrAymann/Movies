37b9d2e9e80cb87a80d893de9431cf42256a1f33
Ibrahim <iibbrraahhimm00@gmai;.com>
2023-11-11 21:32:05 +0200

    finish popular view with mini issues

--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:movies/core/network_layer/api_manager.dart';
+import 'package:movies/pages/home/widgets/popular_movie_item.dart';
+import '../../models/Popular_model.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Column(
       children: [
-        Center(
-          child: Text(
-            'Home View',
-            style: TextStyle(
-                color: Color(0xffB5B4B4),
-                fontSize: 24,
-                fontWeight: FontWeight.bold),
-          ),
-        )
+        FutureBuilder(
+          future: ApiManager.fetchPopular(),
+          builder: (context, snapshot) {
+            if(snapshot.hasError){
+              return Center(
+                child: Text(
+                    snapshot.error.toString()
+                ),
+              );
+            }
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            Results results = snapshot.data!;
+            return Expanded(
+                child: PopularMovieItem(results: results!)
+            );
+          },
+        ),
+
       ],
     );
   }
