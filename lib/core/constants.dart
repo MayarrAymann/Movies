d0883df72363c7c9454c747efdbc654e0dbc3ba2
basta <125200508@sha.edu.eg>
2023-11-12 20:12:30 +0200

    add discover movies endpoint

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -4,4 +4,6 @@ class Constants {
   static const String apiKey = '7bfad8b3e1221a197a2bc66354e68c0c';
 
   static const String categoriesEndPoint = '/3/genre/movie/list';
+
+  static const String discoverMoviesEndPoint = '/3/discover/movie';
 }
