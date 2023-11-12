6a20af8433c7b192e8e0a9c4825d901f103a871a
basta <125200508@sha.edu.eg>
2023-11-12 20:12:07 +0200

    create Movie model

---
+++ lib/pages/browse/widgets/movie_item.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:movies/models/movie_model.dart';
+
+class MovieItem extends StatelessWidget {
+  final MovieModel model;
+
+  const MovieItem({
+    super.key,
+    required this.model,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 360,
+          height: 90,
+          child: Row(
+            children: [
+              Container(
+                width: 140,
+                height: 90,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  image: DecorationImage(
+                    image: NetworkImage(model.posterPath!),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      model.title!,
+                      style: const TextStyle(
+                        fontFamily: 'Inter',
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      model.releaseDate!,
+                      style: const TextStyle(
+                        fontFamily: 'Inter',
+                        color: Colors.white60,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 13,
+                      ),
+                    ),
+                    Text(
+                      model.originalLanguage!,
+                      style: const TextStyle(
+                        fontFamily: 'Inter',
+                        color: Colors.white60,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(10),
+          width: double.infinity,
+          child: const Divider(
+            height: 2,
+            color: Color(0xff707070),
+          ),
+        ),
+      ],
+    );
+  }
+}
