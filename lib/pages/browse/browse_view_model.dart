ddfa7aba60cac85b4531751b2521016145836104
basta <125200508@sha.edu.eg>
2023-11-12 20:14:29 +0200

    add function to get movies filtered by genre id {not finished}

--- lib/pages/browse/browse_view_model.dart
+++ lib/pages/browse/browse_view_model.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
 
 import '../../core/network_layer/api_manager.dart';
-import '../../models/category_model.dart';
+import '../../models/genre_model.dart';
+import '../../models/movie_model.dart';
 
 class BrowseViewModel extends ChangeNotifier {
   List<GenreModel> _genres = [];
+  List<MovieModel> _movies = [];
+  bool _isGenreSelected = false;
 
   List<GenreModel> get genres => _genres;
+
+  List<MovieModel> get movies => _movies;
+
+  bool get isGenreSelected => _isGenreSelected;
+
+  selectedGenre(bool value) {
+    _isGenreSelected = value;
+    notifyListeners();
+  }
 
   getGenres() async {
     try {
       var response = await ApiManager.fetchCategories();
       _genres = response.genres;
+      notifyListeners();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  getMovies(int selectedGenreId) async {
+    try {
+      var response =
+          await ApiManager.discoverMoviesByGenre(genreId: selectedGenreId);
+      _movies = response.results!;
       notifyListeners();
     } catch (e) {
       print(e.toString());
