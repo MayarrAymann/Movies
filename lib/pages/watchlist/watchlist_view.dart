9555ecb3cf3f33099fa3ab2a0b591b2c409f6c5a
MayarrAymann <mayarraymannn@gmail.com>
2023-11-13 14:31:02 +0200

    finish watchlist view using static data

--- lib/pages/watchlist/watchlist_view.dart
+++ lib/pages/watchlist/watchlist_view.dart
@@ -1,50 +1,86 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:movies/core/network_layer/firebase_utils.dart';
+import 'package:movies/models/movie_model.dart';
 import 'package:movies/models/watchlist_model.dart';
 import 'package:movies/pages/watchlist/widgets/watchlist_item.dart';
 
-class WatchListView extends StatelessWidget {
-  WatchListView({super.key});
+import '../../core/constants.dart';
 
-  List<WatchlistItem> items = [
-    WatchlistItem(
-      watchlistModel: WatchlistModel(
-          title: 'Alita Battle Angel',
-          year: '2019',
-          actors: 'Rosa Salazar, Christoph Waltz',
-          imagePath: 'assets/images/MovieCover.png'),
-    ),
-    WatchlistItem(
-      watchlistModel: WatchlistModel(
-          title: 'Alita Battle Angel',
-          year: '2019',
-          actors: 'Rosa Salazar, Christoph Waltz',
-          imagePath: 'assets/images/MovieCover.png'),
-    ),
-    WatchlistItem(
-      watchlistModel: WatchlistModel(
-          title: 'Alita Battle Angel',
-          year: '2019',
-          actors: 'Rosa Salazar, Christoph Waltz',
-          imagePath: 'assets/images/MovieCover.png'),
-    ),
-    WatchlistItem(
-      watchlistModel: WatchlistModel(
-          title: 'Alita Battle Angel',
-          year: '2019',
-          actors: 'Rosa Salazar, Christoph Waltz',
-          imagePath: 'assets/images/MovieCover.png'),
-    ),
-    WatchlistItem(
-      watchlistModel: WatchlistModel(
-          title: 'Alita Battle Angel',
-          year: '2019',
-          actors: 'Rosa Salazar, Christoph Waltz',
-          imagePath: 'assets/images/MovieCover.png'),
-    ),
-  ];
+class WatchListView extends StatefulWidget {
+  const WatchListView({super.key});
+
+  @override
+  State<WatchListView> createState() => _WatchListViewState();
+}
+
+class _WatchListViewState extends State<WatchListView> {
+  @override
+  void initState() {
+    super.initState();
+    FirestoreUtils.addDataToFirestore(
+      MovieModel(
+        firestoreId: '1',
+        adult: false,
+        backdropPath: '${Constants.imageUrl}/h56edmERPTkey89SqyKu4hINVNy.jpg',
+        genreIds: [28, 53],
+        id: 575264,
+        originalLanguage: 'en',
+        originalTitle: 'Mission: Impossible - Dead Reckoning Part One',
+        overview: 'Seventh film in the Mission: Impossible series.',
+        popularity: 2820.303,
+        posterPath: '${Constants.imageUrl}/NNxYkU70HPurnNCSiCjYAmacwm.jpg',
+        releaseDate: '2023-07-08',
+        title: 'Mission: Impossible - Dead Reckoning Part One',
+        video: false,
+        voteAverage: 7.7,
+        voteCount: 2226,
+      ),
+    );
+    FirestoreUtils.addDataToFirestore(
+      MovieModel(
+        firestoreId: '1',
+        adult: false,
+        backdropPath: '${Constants.imageUrl}/h56edmERPTkey89SqyKu4hINVNy.jpg',
+        genreIds: [28, 53],
+        id: 575264,
+        originalLanguage: 'en',
+        originalTitle: 'Mission: Impossible - Dead Reckoning Part One',
+        overview: 'Seventh film in the Mission: Impossible series.',
+        popularity: 2820.303,
+        posterPath: '${Constants.imageUrl}/NNxYkU70HPurnNCSiCjYAmacwm.jpg',
+        releaseDate: '2023-07-08',
+        title: 'Mission: Impossible - Dead Reckoning Part One',
+        video: false,
+        voteAverage: 7.7,
+        voteCount: 2226,
+      ),
+    );
+    FirestoreUtils.addDataToFirestore(
+      MovieModel(
+        firestoreId: '1',
+        adult: false,
+        backdropPath: '${Constants.imageUrl}/h56edmERPTkey89SqyKu4hINVNy.jpg',
+        genreIds: [28, 53],
+        id: 575264,
+        originalLanguage: 'en',
+        originalTitle: 'Mission: Impossible - Dead Reckoning Part One',
+        overview: 'Seventh film in the Mission: Impossible series.',
+        popularity: 2820.303,
+        posterPath: '${Constants.imageUrl}/NNxYkU70HPurnNCSiCjYAmacwm.jpg',
+        releaseDate: '2023-07-08',
+        title: 'Mission: Impossible - Dead Reckoning Part One',
+        video: false,
+        voteAverage: 7.7,
+        voteCount: 2226,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+
     return Padding(
       padding: const EdgeInsets.only(top: 70, left: 15, right: 15),
       child: Column(
@@ -60,9 +96,58 @@ class WatchListView extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) => items[index],
-              itemCount: items.length,
+            child: StreamBuilder<QuerySnapshot<MovieModel>>(
+              stream: FirestoreUtils.getRealTimeDataFromFirestore(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(snapshot.error.toString()),
+                      /*
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => FirestoreUtils.(),
+                    child: const Text('Retry'),
+                  ),*/
+                    ],
+                  );
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                var MoviesList = snapshot.data?.docs
+                        .map((element) => element.data())
+                        .toList() ??
+                    [];
+                return (MoviesList.isEmpty)
+                    ? const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Center(
+                            child: Text(
+                              'WatchList View',
+                              style: TextStyle(
+                                  color: Color(0xffB5B4B4),
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        ],
+                      )
+                    : ListView.builder(
+                        itemBuilder: (context, index) => WatchlistItem(
+                          watchlistModel: WatchlistModel(
+                            title: MoviesList[index].title!,
+                            year: MoviesList[index].releaseDate!,
+                            actors: MoviesList[index].originalTitle!,
+                            imagePath: MoviesList[index].posterPath!,
+                          ),
+                        ),
+                        itemCount: MoviesList.length,
+                      );
+              },
             ),
           ),
         ],
