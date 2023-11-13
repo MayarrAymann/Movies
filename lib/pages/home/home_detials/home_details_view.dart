3b24b71e095578fc7192bbfef8a642445392f699
Kholoudd <kholoudemad551@gmail.com>
2023-11-13 02:24:31 +0200

    home details start

---
+++ lib/pages/home/home_detials/home_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeDetailsView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:  Text("home details " , style:  TextStyle(fontSize: 30),),
+    );
+  }
+
+}
