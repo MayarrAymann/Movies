73744feb99c10d6d6473f026bcd5b2fb80cd146b
basta <125200508@sha.edu.eg>
2023-11-13 13:18:04 +0200

    create search builder and delegate

---
+++ lib/pages/search/widgets/news_builder.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter/cupertino.dart';
+//
+// class searchBuilder extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//    return  FutureBuilder( builder: (context , snapshot){
+//
+//    },
+//    );
+//   }
+//
+// }
