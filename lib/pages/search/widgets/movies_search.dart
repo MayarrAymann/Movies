51737937260a64daaa8df2f43b67fd6e468cb477
basta <125200508@sha.edu.eg>
2023-11-14 16:05:03 +0200

    finish search delegate class to handle search process

--- lib/pages/search/widgets/movies_search.dart
+++ lib/pages/search/widgets/movies_search.dart
@@ -1,27 +1,56 @@
 import 'package:flutter/material.dart';
 
+import '../search_view_model.dart';
+
 class MoviesSearch extends SearchDelegate {
+  final SearchViewModel vm;
+
+  MoviesSearch({required this.vm});
+
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      IconButton(
+        onPressed: () => showResults(context),
+        icon: const Icon(Icons.search),
+      )
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return IconButton(
+      onPressed: () => Navigator.pop(context),
+      icon: const Icon(Icons.close),
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
+    vm.getMovies(query);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pop(context);
+    });
+
+    return const Column(
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                color: Color(0xffB5B4B4),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return Container();
   }
 }
