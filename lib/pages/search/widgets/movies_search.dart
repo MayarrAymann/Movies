1b3b0e4a8e745e6c57f2ec0300ca717033d677c3
basta <125200508@sha.edu.eg>
2023-11-14 16:44:26 +0200

    handle search appTheme

--- lib/pages/search/widgets/movies_search.dart
+++ lib/pages/search/widgets/movies_search.dart
@@ -53,4 +53,42 @@ class MoviesSearch extends SearchDelegate {
   Widget buildSuggestions(BuildContext context) {
     return Container();
   }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final ColorScheme colorScheme = theme.colorScheme;
+    return ThemeData(
+      appBarTheme: AppBarTheme(
+        backgroundColor: Color(0xff514F4F).withOpacity(0.5),
+        elevation: 0,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+        border: InputBorder.none,
+      ),
+    );
+  }
 }
+/*
+theme.copyWith(
+      appBarTheme: AppBarTheme(
+        systemOverlayStyle: colorScheme.brightness == Brightness.dark
+            ? SystemUiOverlayStyle.light
+            : SystemUiOverlayStyle.dark,
+        backgroundColor: colorScheme.brightness == Brightness.dark ? Colors.grey[900] : Colors.white,
+        iconTheme: theme.primaryIconTheme.copyWith(color: Colors.grey),
+        titleTextStyle: theme.textTheme.titleLarge,
+        toolbarTextStyle: theme.textTheme.bodyMedium,
+      ),
+      inputDecorationTheme: searchFieldDecorationTheme ??
+          InputDecorationTheme(
+            hintStyle: searchFieldStyle ?? theme.inputDecorationTheme.hintStyle,
+            border: InputBorder.none,
+          ),
+    );
+ */
