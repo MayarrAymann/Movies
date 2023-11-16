b88e53da1953ce8a2f976a8956458ec67ef56b06
basta <125200508@sha.edu.eg>
2023-11-16 22:53:37 +0200

    finish popular movies section

--- lib/pages/home/widgets/popular_movie_item.dart
+++ lib/pages/home/widgets/popular_movie_item.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:movies/core/constants.dart';
-import 'package:movies/models/movie_model.dart';
 
+import '../../../core/constants.dart';
+import '../../../models/details_model.dart';
 import '../home_detials/home_details_view.dart';
 
 class PopularMovieItem extends StatelessWidget {
-  final MovieModel model;
+  final DetailsModel model;
 
   const PopularMovieItem({Key? key, required this.model}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var mediaQuery = MediaQuery.of(context);
+    var width = mediaQuery.size.width;
+    var height = mediaQuery.size.height;
 
     return SafeArea(
       child: Column(
@@ -20,17 +23,27 @@ class PopularMovieItem extends StatelessWidget {
           Expanded(
             child: Stack(
               children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Image.network(
-                      "${Constants.imageBaseURL}${model.backdropPath}",
-                      fit: BoxFit.cover,
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      HomeDetailsView.routeName,
+                      arguments: model,
+                    );
+                  },
+                  child: Container(
+                    width: width,
+                    height: 250,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "${Constants.imageBaseURL}${model.backdropPath}"),
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                    GestureDetector(
-                        onTap: (){},
-                        child: Image.asset("assets/images/play_button.png")),
-                  ],
+                    child: Image.asset("assets/images/play_button.png"),
+                  ),
                 ),
                 Positioned(
                   bottom: -20,
@@ -39,13 +52,27 @@ class PopularMovieItem extends StatelessWidget {
                     children: [
                       Stack(
                         children: [
-                          Image.network(
-                            "${Constants.imageBaseURL}${model.posterPath}",
-                            width: 140,
-                            height: 250,
-                            fit: BoxFit.cover,
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                HomeDetailsView.routeName,
+                                arguments: model,
+                              );
+                            },
+                            child: Image.network(
+                              "${Constants.imageBaseURL}${model.posterPath}",
+                              width: 140,
+                              height: 250,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                          Image.asset("assets/images/add_to_bookmark.png"),
+                          GestureDetector(
+                              onTap: () {
+                                print('bookmarkPressed');
+                              },
+                              child: Image.asset(
+                                  "assets/images/add_to_bookmark.png")),
                         ],
                       ),
                       Padding(
@@ -59,12 +86,24 @@ class PopularMovieItem extends StatelessWidget {
                               softWrap: true,
                             ),
                             const SizedBox(height: 10),
-                            Text(
-                              model.releaseDate ?? "",
-                              style: theme.textTheme.bodyLarge!.copyWith(
-                                fontSize: 10,
-                                color: const Color(0XFFB5B4B4)
-                              ),
+                            Row(
+                              children: [
+                                Text(
+                                  Constants.getMovieReleaseYear(
+                                      model.releaseDate!),
+                                  style: theme.textTheme.bodyLarge!.copyWith(
+                                      fontSize: 10,
+                                      color: const Color(0XFFB5B4B4)),
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  Constants.getMovieDuration(
+                                      model.runtime ?? 0),
+                                  style: theme.textTheme.bodyLarge!.copyWith(
+                                      fontSize: 10,
+                                      color: const Color(0XFFB5B4B4)),
+                                ),
+                              ],
                             ),
                           ],
                         ),
