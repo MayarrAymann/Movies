fa80bca31fc771300a885de5157033d1ebbb6a9d
basta <125200508@sha.edu.eg>
2023-11-16 18:12:41 +0200

    add more end points

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
-  static const String baseURL = 'api.themoviedb.org';
-
   static const String apiKey = '7bfad8b3e1221a197a2bc66354e68c0c';
+
+  static const String baseURL = 'api.themoviedb.org';
 
   static const String categoriesEndPoint = '/3/genre/movie/list';
 
@@ -9,7 +9,11 @@ class Constants {
 
   static const String searchEndPoint = '/3/search/movie';
 
+  static const String popularEndPoint = '/3/movie/popular';
+
+  static const String newReleaseEndPoint = '/3/movie/upcoming';
+
+  static const String topRatedEndPoint = '/3/movie/top_rated';
+
   static const String imageBaseURL = 'https://image.tmdb.org/t/p/w500';
-
-
 }
