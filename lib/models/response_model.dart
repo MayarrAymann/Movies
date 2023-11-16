ff5a60b8feb1f931a9a33aed518d4d05d0bbff48
basta <125200508@sha.edu.eg>
2023-11-16 18:08:37 +0200

    add dates parameter which returns from Upcoming Movies API

--- lib/models/response_model.dart
+++ lib/models/response_model.dart
@@ -1,12 +1,14 @@
 import 'movie_model.dart';
 
 class ResponseModel {
+  Map<String, String>? dates;
   int? page;
   List<MovieModel>? results;
   int? totalPages;
   int? totalResults;
 
   ResponseModel({
+    this.dates,
     this.page,
     this.results,
     this.totalPages,
@@ -15,6 +17,7 @@ class ResponseModel {
 
   factory ResponseModel.fromJson(Map<String, dynamic> json) {
     return ResponseModel(
+      dates: json['dates'] ?? {},
       page: json['page'],
       results:
           (json['results'] as List).map((e) => MovieModel.fromJson(e)).toList(),
